39ed805d11fc4935c142c0e0fd02a71a65d94587
EddieKeller <116ekg@gmail.com>
2017-08-02 17:42:11 -0700

    added db address to gitignore

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
+const DBcred = require('../../DBcred.js')
 
-const db = new Sequelize('postgres://skugxhjk:D5LvwMq8ZkwUfU_QBBMYAmrl_PY1sTOc@babar.elephantsql.com:5432/skugxhjk', {
+const db = new Sequelize('DBcred', {
   dialect: 'postgres'
 })
 console.log('now connected to remote db!')
